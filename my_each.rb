89ccee97c13ffaa99bab61ded29aef9781446d38
Alexander <acepetrov96@gmail.com>
2020-10-23 13:41:12 -0500

    commit tree 9e3e439cd7b44b497577baf9c2ab9f494899572c

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+  int = 0
+  while int < collection.length
+    yield(collection[int])
+    int += 1
+  end
+
+  collection
 end
